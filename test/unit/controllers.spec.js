5059a2d2da4c4224e31771e99ed2cd9a0afc85a4
Robin Heathcote <robin.heathcote@icloud.com>
2016-06-06 10:32:57 +0100

    All tests pass

--- test/unit/controllers.spec.js
+++ test/unit/controllers.spec.js
@@ -2,18 +2,19 @@ describe("NewsSummaryController", function() {
   beforeEach(module("newsSummaryApp"));
 
   var controller, httpBackend;
-  var headlines = [{headline: 'alex-salmond-speech-first-minister-scottish-independence-eu-currency'}];
+  var headlines = {response: {results: [{headline: 'alex-salmond-speech-first-minister-scottish-independence-eu-currency'}]}};
 
   beforeEach(inject(function($httpBackend, $controller) {
     controller = $controller("NewsSummaryController");
     httpBackend = $httpBackend;
-    httpBackend.expectGET("http://news-summary-api.herokuapp.com/").respond(headlines);
+    httpBackend.expectGET("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/football?").respond(headlines);
     httpBackend.flush();
   }));
 
   it('fetches a headlines', function(){
     var headline1 = {headline: 'alex-salmond-speech-first-minister-scottish-independence-eu-currency'};
     controller.getAll().then(function(headlines){
+      console.log(headlines)
       expect(controller.headlines).toEqual([headline1]);
     });
   });
